418fc3b0d0ce971eb3fbf91166946d80d5b75074
Nelson PIRES DA SILVA <piresdasilva.n@gmail.com>
2022-09-05 18:51:39 +0200

    Fini
    Problème menu burger done

--- intro-section-with-dropdown-navigation-main/script.js
+++ intro-section-with-dropdown-navigation-main/script.js
@@ -1,49 +1,55 @@
+let media500            = window.matchMedia("(max-width: 500px)");
+let media1000           = window.matchMedia("(min-width: 1000px)");
+
 let features            = document.querySelector("#features");
 let arrowDownFeatures   = document.querySelector('#arrow_down_features');
 let arrowUpFeatures     = document.querySelector("#arrow_up_features");
 
 let company             = document.querySelector("#company");
-let arrowDownCompany   = document.querySelector('#arrow_down_company');
-let arrowUpCompany     = document.querySelector("#arrow_up_company");
+let arrowDownCompany    = document.querySelector('#arrow_down_company');
+let arrowUpCompany      = document.querySelector("#arrow_up_company");
 
 let todo                = document.querySelector("#todo_list");
 let history             = document.querySelector("#history");
 let hidden              = true;
 
 features.addEventListener("click", () => {
-    if(hidden){
-        todo.style.display = "flex";
-        arrowDownFeatures.style.display = "none";
-        arrowUpFeatures.style.display = "flex";
-        features.style.marginBottom = "160px";
-        history.style.top = "290px";
-        hidden = false;
-    }
-    else{
-        todo.style.display = "none";
-        arrowDownFeatures.style.display = "flex";
-        arrowUpFeatures.style.display = "none";
-        features.style.marginBottom = "0px";
-        history.style.top = "130px";
-        hidden = true;
-    }
+
+        if(hidden){
+            todo.style.display = "flex";
+            arrowDownFeatures.style.display = "none";
+            arrowUpFeatures.style.display = "flex";
+            // features.style.marginBottom = "160px";
+            // history.style.top = "290px";
+            hidden = false;
+        }
+        else{
+            todo.style.display = "none";
+            arrowDownFeatures.style.display = "flex";
+            arrowUpFeatures.style.display = "none";
+            // features.style.marginBottom = "0px";
+            // history.style.top = "130px";
+            hidden = true;
+        }
 });
 
 company.addEventListener("click", () => {
-    if(hidden){
-        history.style.display = "flex";
-        arrowDownCompany.style.display = "none";
-        arrowUpCompany.style.display = "flex";
-        company.style.marginBottom = "125px";
-        hidden = false;
-    }
-    else{
-        history.style.display = "none";
-        arrowDownCompany.style.display = "flex";
-        arrowUpCompany.style.display = "none";
-        company.style.marginBottom = "0px";
-        hidden = true;
-    }
+
+        if(hidden){
+            history.style.display = "flex";
+            arrowDownCompany.style.display = "none";
+            arrowUpCompany.style.display = "flex";
+            // company.style.marginBottom = "125px";
+            hidden = false;
+        }
+        else{
+            history.style.display = "none";
+            arrowDownCompany.style.display = "flex";
+            arrowUpCompany.style.display = "none";
+            // company.style.marginBottom = "0px";
+            hidden = true;
+        }
+    
 });
 
 // MOBILE
@@ -54,31 +60,89 @@ let bar             = document.querySelector("#bar");
 let cross           = document.querySelector("#cross");
 
 menuBurger.addEventListener("click", () => {
-    if(hidden){
-        all.style.display       = "flex";
-        bar.style.display       = "none";
-        cross.style.display     = "flex";
-        history.style.display   = "flex";
+    if(media500.matches){
 
-        todo.style.display   = "none";
-        history.style.display = "none";
-        arrowDownCompany.style.display = "flex";
-        arrowUpCompany.style.display = "none";
-        arrowDownFeatures.style.display = "flex";
-        arrowUpFeatures.style.display = "none";
+        if(hidden){
+            bar.style.display = "none";
+            cross.style.display = "flex";
 
-        features.style.marginBottom = "0px";
-        company.style.marginBottom = "0px";
+            all.style.display = "flex";
 
-        hidden = false;
-    }
-    else{
-        all.style.display = "none";
-        bar.style.display   = "flex";
-        cross.style.display = "none";
-        history.style.display   = "none";
-        todo.style.display   = "none";
-        hidden = true;
+                features.addEventListener("click", () => {
+
+                    if(hidden){
+
+                        arrowDownFeatures.style.display = "none";
+                        arrowUpFeatures.style.display = "flex";
+                        features.style.marginBottom = "160px";
+                        todo.style.display = "flex";
+
+                        history.style.top = "290px";
+
+                        hidden = true;
+                    }
+                    else{
+                        arrowDownFeatures.style.display = "flex";
+                        arrowUpFeatures.style.display = "none";
+                        features.style.marginBottom = "0px";
+                        todo.style.display = "none";
+
+                        history.style.top = "130px";
+
+                        hidden = false;
+                    }
+                });
+
+                company.addEventListener("click", () => {
+
+                    if(hidden){
+
+                        arrowDownCompany.style.display = "none";
+                        arrowUpCompany.style.display = "flex";
+                        company.style.marginBottom = "125px";
+
+                        history.style.display = "flex";
+    
+                        hidden = true;
+                    }
+                    else{
+                        arrowDownCompany.style.display = "flex";
+                        arrowUpCompany.style.display = "none";
+                        company.style.marginBottom = "0px";
+
+                        history.style.display = "none";
+
+                        hidden = false;
+                    }
+                });
+            
+
+            hidden = false;
+        }
+
+        else{
+
+            history.style.top = "130px";
+            
+            bar.style.display = "flex";
+            cross.style.display = "none";
+
+            all.style.display = "none";
+
+            features.style.marginBottom = "0px";
+            company.style.marginBottom = "0px";
+
+            todo.style.display = "none";
+            history.style.display = "none";
+
+            arrowDownFeatures.style.display = "flex";
+            arrowUpFeatures.style.display = "none";
+
+            arrowDownCompany.style.display = "flex";
+            arrowUpCompany.style.display = "none";
+
+            hidden = true;
+        }
     }
 });
 
