b0079a5d3ebc96bfc845193acae49e3abb2df9ac
Nelson PIRES DA SILVA <piresdasilva.n@gmail.com>
2022-09-05 06:16:43 +0200

    Test
    test

--- intro-section-with-dropdown-navigation-main/script.js
+++ intro-section-with-dropdown-navigation-main/script.js
@@ -15,12 +15,16 @@ features.addEventListener("click", () => {
         todo.style.display = "flex";
         arrowDownFeatures.style.display = "none";
         arrowUpFeatures.style.display = "flex";
+        features.style.marginBottom = "160px";
+        history.style.top = "290px";
         hidden = false;
     }
     else{
         todo.style.display = "none";
         arrowDownFeatures.style.display = "flex";
         arrowUpFeatures.style.display = "none";
+        features.style.marginBottom = "0px";
+        history.style.top = "130px";
         hidden = true;
     }
 });
@@ -30,12 +34,51 @@ company.addEventListener("click", () => {
         history.style.display = "flex";
         arrowDownCompany.style.display = "none";
         arrowUpCompany.style.display = "flex";
+        company.style.marginBottom = "125px";
         hidden = false;
     }
     else{
         history.style.display = "none";
         arrowDownCompany.style.display = "flex";
         arrowUpCompany.style.display = "none";
+        company.style.marginBottom = "0px";
         hidden = true;
     }
 });
+
+// MOBILE
+let body            = document.querySelector("body");
+let menuBurger      = document.querySelector("#menu_burger");
+let all             = document.querySelector("#all");
+let bar             = document.querySelector("#bar");
+let cross           = document.querySelector("#cross");
+
+menuBurger.addEventListener("click", () => {
+    if(hidden){
+        all.style.display       = "flex";
+        bar.style.display       = "none";
+        cross.style.display     = "flex";
+        history.style.display   = "flex";
+
+        todo.style.display   = "none";
+        history.style.display = "none";
+        arrowDownCompany.style.display = "flex";
+        arrowUpCompany.style.display = "none";
+        arrowDownFeatures.style.display = "flex";
+        arrowUpFeatures.style.display = "none";
+
+        features.style.marginBottom = "0px";
+        company.style.marginBottom = "0px";
+
+        hidden = false;
+    }
+    else{
+        all.style.display = "none";
+        bar.style.display   = "flex";
+        cross.style.display = "none";
+        history.style.display   = "none";
+        todo.style.display   = "none";
+        hidden = true;
+    }
+});
+
